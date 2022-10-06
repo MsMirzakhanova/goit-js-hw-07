1029ff5aba093330cfbe7e906e4be0bf115d9569
MsMirzakhanova <mirzakhanovamari@gmail.com>
2022-10-06 13:38:48 +0300

    task 1 (without add.functions )

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,47 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+//console.log(galleryItems);
+
+const galleryContainer = document.querySelector(`.gallery`);
+//console.log(galleryContainer);
+const imgMarkup = createImgMarkup(galleryItems);
+//console.log(imgMarkup);
+
+galleryContainer.insertAdjacentHTML(`beforeend`, imgMarkup);
+
+function createImgMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+<div class="gallery__item">
+  <a class="gallery__link" 
+  href= "${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+   `;
+    })
+    .join(``);
+}
+
+galleryContainer.addEventListener("click", onGalleryContainerClick);
+
+function onGalleryContainerClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const url = event.target.getAttribute("data-source");
+  //console.log(url);
+  const instance = basicLightbox.create(`
+    <img src="${url}" width="800" height="600">
+`);
+
+  instance.show();
+}
