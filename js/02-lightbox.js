cec7fd581a22379c0a47dd300882ba1527f438c5
MsMirzakhanova <98583249+MsMirzakhanova@users.noreply.github.com>
2022-10-07 13:22:58 +0300

    task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,4 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
@@ -14,19 +14,19 @@ function createImgMarkup(galleryItems) {
   return galleryItems
     .map(({ preview, original, description }) => {
       return `
-<div class="gallery__item">
-  <a class="gallery__link" 
-  href= "${original}">
-    <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-    />
-  </a>
-</div>
+<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
    `;
     })
     .join(``);
 }
 
+
+let lightbox = new SimpleLightbox('.gallery a', { 
+    /* options */ 
+    captionsData: "alt",
+    captionDelay: 250,
+    captionPosition: "bottom",
+
+});
